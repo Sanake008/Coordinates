c8688779524251ad0945436642f65151dc6c43ed
Sanake008 <mcm-mb@hotmail.com>
2019-10-03 19:39:39 -0500

    Ajustes

--- main.cpp
+++ main.cpp
@@ -17,8 +17,8 @@ struct Vector3
 
 /*Esta es una anotacion */
 
-void Addition(Vector3 a, Vector3 b, Vector3 *res);
-Vector3 Subtraction (Vector3 a, Vector3 b);
+void Addition(Vector3 *a, Vector3 *b, Vector3 *res);
+Vector3 Direction (Vector3 *a, Vector3 *b, Vector3 *res);
 void Printvector(Vector3 *v);
 
 int main()
@@ -26,17 +26,27 @@ int main()
     Vector3 a(10,1,13);
     Vector3 b(16,25,30);
     Vector3 res(0,0,0);
-    Addition(a,b,&res);
+    Addition(&a,&b,&res);
     Printvector(&res);
+    cout << "Plataforma de " << 8*sizeof(int) << " bits";
     return 0;
 }
 
-void Addition(Vector3 a, Vector3 b, Vector3 *res)
+void Addition(Vector3 *a, Vector3 *b, Vector3 *res)
 {
-    res->x = a.x + b.x;
-    res->y = a.y + b.y;
-    res->z = a.z + b.z;
+    res->x = a->x + b->x;
+    res->y = a->y + b->y;
+    res->z = a->z + b->z;
 }
+
+Vector3 Direction(Vector3 *a, Vector3 *b, Vector3 *res)
+{
+    res->x = b->x - a->x;
+    res->y = b->y - a->y;
+    res->z = b->z - a->z;
+}
+
+
 void Printvector(Vector3 *v)
 {
     cout<<"This is the vector: ("<<v->x <<","<<v->y<<","<< v->z<<")"<<endl;
